344febd66095c87f1891f26f6a15ed2d2a515d93
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2021-03-04 10:45:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ with dataExploration:
     st.header("Let's Explore Our Data!")
     st.write("In this section let us explore our data!")
     diamonds_data = pd.read_csv('diamonds.csv')
-# Note : This does not work on Streamlit sharing as of now. But I am going to fix it
-#     profile = ProfileReport(diamonds_data) # Generaing the report
-#     st_profile_report(profile) # Actually displaying it on screen
+Note : This does not work on Streamlit sharing as of now. But I am going to fix it
+    profile = ProfileReport(diamonds_data) # Generaing the report
+    st_profile_report(profile) # Actually displaying it on screen
     
     fig = sns.pairplot(diamonds_data)
     st.pyplot(fig)
